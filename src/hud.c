c79d61961a8548945201e0ec356f53b6dcc66c08
marcoskusa <makusano@umag.cl>
2026-07-12 18:36:44 -0300

    camibios al hud

--- src/hud.c
+++ src/hud.c
@@ -18,11 +18,12 @@ void dibuMenu(HUD *hud, Jugador *j){
     al_draw_rectangle(xMenu,0,xMenu+anchoP,altoP,al_map_rgb(255,255,255),2);
 
     al_draw_text(hud->fuente,al_map_rgb(255,255,255),xMenu+20,20,0,"JUGADOR");
+
     al_draw_textf(hud->fuente,al_map_rgb(255,80,80), xMenu+20,60,0,"Vida: %d",j->vida);
 
-    al_draw_textf(hud->fuente,al_map_rgb(255,80,80), xMenu+20,60,0,"Oro: %d",j->oro);
+    al_draw_textf(hud->fuente,al_map_rgb(255,80,80), xMenu+20,90,0,"Oro: %d",j->oro);
 
-    al_draw_textf(hud->fuente,al_map_rgb(255,80,80), xMenu+20,60,0,"Madera: %d",j->madera);
+    al_draw_textf(hud->fuente,al_map_rgb(255,80,80), xMenu+20,120,0,"Madera: %d",j->madera);
 
 
 }
