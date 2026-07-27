2723958a2dc6d1fc261b04fefca34e909e6bc1a7
marcoskusa <makusano@umag.cl>
2026-07-27 16:31:33 -0300

    struct de bala y funciones basicas

--- src/bala.c
+++ src/bala.c
@@ -1,18 +1,57 @@
 #include "bala.h"
 
-void inicBala(){
+void inicBala(Bala balas[], int cantidadBalas, ALLEGRO_BITMAP * sprite){
     for (int i =0 ; i<cantidadBalas;i++)
     {
-        balas[i].activa=;
-        balas[i].x=;
-        balas[i].y=;
-        balas[i].velocX=;
-        balas[i].velocY=;
+        balas[i].activo=false;
+        balas[i].x=0;
+        balas[i].y=0;
+        balas[i].velocidad = 10;
+        balas[i].ancho= 8;
+        balas[i].alto= 8;
+        balas[i].objetivo = -1;
+        balas[i].dano = 0;
+        balas[i].sprite =sprite;
     }
 }
-void dibuBala(){
+bool dispararBala(Bala balas[], int cantidadBalas, Torre *torre,Enemigo enemigos[],int indiceEnemigo){
+    
+}
+
+void dibuBala(Bala balas[], int cantidadBalas){
+    for(int i = 0 ; i<cantidadBalas; i++){
+        if(!balas[i].activo){
+            continue;
+        }
+        if(balas[i].sprite!=NULL){
+            al_draw_scaled_bitmap(balas[i].sprite,0,0,al_get_bitmap_width(balas[i].sprite ),al_get_bitmap_height(balas[i].sprite),balas[i].x,balas[i].y,balas[i].ancho,balas[i].alto,0);
+        }
+    }
 
 }
-void actBala(){
+void actBalas(Bala balas[], int cantidadBalas,Enemigo enemigos[], int cantidadEnemigos ){
+    for(int i =0; i< cantidadBalas; i++)
+    {
+        Bala *bala = &balas[i];
 
+        if(!bala->activo){
+            continue;
+        }
+        if(bala->objetivo < 0 || bala -> objetivo >= cantidadEnemigos){
+            bala->activo = false;
+            bala ->objetivo = -1;
+            continue;
+        }
+        Enemigo *objetivo = &enemigos[bala->objetivo];
+        if(!objetivo->vivo){
+            bala->activo = false;
+            bala ->objetivo = -1;
+            continue;
+        }
+
+
+
+
+
+    }
 }
--- src/bala.h
+++ src/bala.h
@@ -2,14 +2,23 @@
 #define Bala_h
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <stdbool.h>
+
+#include "enemigo.h"
+#include "torre.h"
 
 typedef struct 
 {
-    int x;
-    int y;
+    float x;
+    float y;
 
     int ancho;
     int alto;
+
+    float velocidad; //en x e y
+
+    int dano;
+    int objetivo;
 
     bool activo;
 
@@ -17,9 +26,12 @@ typedef struct
 
 }Bala;
 
-void inicBala(void);
-void dibuBala(void);
-void actBala(void);
+void inicBala(Bala balas[], int cantidadBalas, ALLEGRO_BITMAP * sprite);
+void dispararBala(Bala balas[], int cantidadBalas, Torre *torre, Enemigo enemigos[],int indiceEnemigo);
+
+void actBala(Bala balas[],int cantidadBalas,Enemigo enemigos[],int cantidadEnemigos);
+void dibuBala(Bala balas[],int cantidadBalas);
+
 
 
 #endif
